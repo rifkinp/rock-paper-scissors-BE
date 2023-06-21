eb33947e8c75aeccc7c4f6625b86c07f453a74a8
Rifki Nugraha <rifkichocobo@gmail.com>
2023-06-22 02:26:56 +0800

    tes

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-// index.js
-const express = require("express");
-
-const app = express();
-const PORT = 4000;
-
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
-});
-
-app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
-});
-
-app.get("/about", (req, res) => {
-  res.send("This is my about route..... ");
-});
-
-// Export the Express API
-module.exports = app;
-
+// // index.js
 // const express = require("express");
+
 // const app = express();
-// const port = 8000;
-// const usersRoute = require("./users/user.route");
-// const gameRoute = require("./game/game.route");
-// const cors = require("cors");
-// const swaggerUi = require("swagger-ui-express");
-// const swaggerDocument = require("./gameRpsSwagger.json");
-// require("dotenv").config();
+// const PORT = 4000;
 
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.static("public"));
-
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.listen(PORT, () => {
+//   console.log(`API listening on PORT ${PORT} `);
+// });
 
 // app.get("/", (req, res) => {
-//   return res.json({message: "Hello World"});
+//   res.send("Hey this is my API running 🥳");
 // });
 
-// app.get("/pong", (req, res) => {
-//   return res.json({message: "PINGGG POING"});
+// app.get("/about", (req, res) => {
+//   res.send("This is my about route..... ");
 // });
 
-// app.use("/games", gameRoute);
+// // Export the Express API
+// module.exports = app;
 
-// app.use("/users", usersRoute);
+const express = require("express");
+const app = express();
+const port = 8000;
+const usersRoute = require("./users/user.route");
+const gameRoute = require("./game/game.route");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./gameRpsSwagger.json");
+require("dotenv").config();
 
-// app.listen(port, () => {
-//   console.log("App is running on port " + port);
-// });
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get("/", (req, res) => {
+  return res.json({message: "Hello World"});
+});
+
+app.get("/pong", (req, res) => {
+  return res.json({message: "PINGGG POING"});
+});
+
+app.use("/games", gameRoute);
+
+app.use("/users", usersRoute);
+
+app.listen(port, () => {
+  console.log("App is running on port " + port);
+});
