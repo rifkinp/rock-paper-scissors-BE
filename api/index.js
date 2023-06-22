f24eea8bee84eb2eb991ef1a5f74007a177d6c80
Rifki Nugraha <rifkichocobo@gmail.com>
2023-06-22 19:28:34 +0800

    ets

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const port = 8000;
 
 require("dotenv").config();
 // app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 // app.use(express.static("public"));
 
 app.get("/", (req, res) => {
