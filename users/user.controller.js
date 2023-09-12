b815f09669c4fc330e003a682c7333d77470024b
Rifki Nugraha <rifkichocobo@gmail.com>
2023-09-12 22:55:07 +0800

    feat : add new route for ease of use

--- users/user.controller.js
+++ users/user.controller.js
@@ -1,113 +1,385 @@
-const userModel = require("./user.model");
-const jwtApp = require("jsonwebtoken");
-const {matchedData} = require("express-validator");
-require("dotenv").config();
+/* eslint-disable no-console */
+/* eslint-disable class-methods-use-this */
+const PDFDocument = require('pdfkit');
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+const userModel = require('./user.model');
+const judgementRPS = require('../utils/judgementValidation');
+const { bucket } = require('../config/firebase');
+require('dotenv').config();
 
-class userController {
-    dataUser = async (req, res) => {
-        try {
-            const allUser = await userModel.getAllUser();
-            return res.json(allUser);
-        } catch (error) {
-            console.log(error);
-            return res
-                .status(500)
-                .json({message: "Error retrieving user data"});
-        }
+class UserController {
+  getAllUser = async (req, res) => {
+    try {
+      const allUser = await userModel.getAllUser();
+      return res.json(allUser);
+    } catch (error) {
+      console.log(error);
+      res.statusCode = 500;
+      return res.json({ message: 'Internal Server Error' });
+    }
+  };
+
+  registerNewUser = async (req, res) => {
+    const userData = req.body;
+    try {
+      const existUser = await userModel.isUserExist(userData);
+      if (existUser) {
+        res.statusCode = 400;
+        return res.json({ message: 'Username atau Email sudah terdaftar' });
+      }
+
+      userModel.registerNewUser(userData);
+      return res.json({ message: 'User Baru telah Terdaftar' });
+    } catch (error) {
+      res.statusCode = 400;
+      console.log(error);
+      return res.json({ message: 'ada error di controller registerUser' });
+    }
+  };
+
+  loginUser = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+      const dataLogin = await userModel.loginUser(username, password);
+
+      if (dataLogin) {
+        // NYALAKAN JWT
+        const token = jwt.sign(dataLogin, process.env.SECRET_KEY);
+
+        // if (dataLogin) {
+        //   // NYALAKAN JWT
+        //   const token = jwt.sign(dataLogin, process.env.SECRET_KEY, {
+        //     expiresIn: '1d',
+        //   });
+
+        return res.json({ accessToken: token });
+      }
+      return res.status(400).json({ message: 'User tidak ditemukan' });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(400)
+        .json({ message: 'Ada yang salah di controller login' });
+    }
+  };
+
+  uploadProfile = async (req, res) => {
+    const token = req.token.id;
+    const { link } = req.body;
+
+    const profileExist = await userModel.isProfileExist(token);
+    try {
+      if (profileExist) {
+        await userModel.updateProfile(link, token);
+        return res.json({ message: 'Profile Updated' });
+      }
+      await userModel.uploadProfile(link, token);
+      return res.json({ message: 'Profile Uploaded' });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(400)
+        .json({ message: 'Ada yang salah di controller uploadProfile' });
+    }
+  };
+
+  // eslint-disable-next-line consistent-return
+  updateUserBio = async (req, res) => {
+    // dapatkan userId dari param
+    const token = req.token.id;
+    const {
+      fullname, address, phoneNumber, profilePicture,
+    } = req.body;
+    // pastikan data yang dimasukan sesuai syarat
+
+    // cek apakah data sudah ada atau belum
+    const bioExist = await userModel.isUserBioExist(token);
+    try {
+      if (bioExist) {
+        // jika userId sudah terdaftar update biodata
+        userModel.updateUserBio(
+          token,
+          fullname,
+          address,
+          phoneNumber,
+          profilePicture,
+        );
+        return res.json({ message: 'Biodata sudah di update' });
+      }
+      // jika userId tidak ada di db buatkan biodata baru
+      userModel.registerUserBio(
+        token,
+        fullname,
+        address,
+        phoneNumber,
+        profilePicture,
+      );
+      return res.json({ message: 'Biodata sudah di buat' });
+    } catch (error) {
+      console.log(error);
+      res.statusCode = 400;
+      res.json({ message: 'error try catch controller updateUserBio' });
+    }
+  };
+
+  findUser = async (req, res) => {
+    const token = req.token.id;
+
+    try {
+      const dataUser = await userModel.findUser(token);
+      return res.json({ dataUser });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(400)
+        .json({ message: ' ada yang salah di controller findUser' });
+    }
+  };
+
+  createRoom = async (req, res) => {
+    const player1 = req.body;
+    const token = req.token.id;
+
+    try {
+      if (
+        player1.choicePlayer1 !== 'batu'
+        && player1.choicePlayer1 !== 'kertas'
+        && player1.choicePlayer1 !== 'gunting'
+      ) {
+        return res
+          .status(400)
+          .json({ message: 'Format harus berupa batu / kertas / gunting' });
+      }
+
+      const createRoom = await userModel.createRoom(player1, token);
+      return res.json({
+        message: `room berhasil di buat, id room: ${createRoom.dataValues.id}`,
+        idRoom: createRoom.dataValues.id,
+      });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(400)
+        .json({ message: 'ada yang salah di controller createRoom' });
+    }
+  };
+
+  joinRoom = async (req, res) => {
+    const { id } = req.params;
+    const player2 = req.body;
+    const token = req.token.id;
+
+    try {
+      if (
+        player2.choicePlayer2 !== 'batu'
+        && player2.choicePlayer2 !== 'kertas'
+        && player2.choicePlayer2 !== 'gunting'
+      ) {
+        return res
+          .status(400)
+          .json({ message: 'Format harus berupa batu / kertas / gunting' });
+      }
+      // cari room berdasarkan ID
+      const singleRoom = await userModel.getSingleRoom(id);
+      // buat agar player tidak melawan dirinya sendiri
+      if (singleRoom.idPlayer1 === token) {
+        return res
+          .status(400)
+          .json({ message: 'Dilarang melawan diri sendiri' });
+      }
+
+      // buat agar player tidak dapat bermain atau mengganti data di room yang sudah selesai
+      if (singleRoom.statusRoom === 'Finish') {
+        res.statusCode = 400;
+        return res.json({ message: 'Room sudah selesai' });
+      }
+
+      // input data pilihan player 2 ke database
+      await userModel.joinRoom(player2, token, id);
+
+      const p1Result = singleRoom.choicePlayer1;
+      const p2Result = player2.choicePlayer2;
+
+      // tentukan pemenang
+      const result = await judgementRPS.judgementRPS(p1Result, p2Result);
+      console.log(result); // ["win", "lose"]
+
+      // masukan result pada database
+      const RoomResult = await userModel.RoomResult(result, id);
+
+      res.json({
+        message: 'Berhasil melawan',
+        'Player 1 Choice': p1Result,
+        'Player 2 Choice': p2Result,
+        'Player 1 Result': result[0],
+        'Player 2 Result': result[1],
+        'Room ID': id,
+      });
+      return RoomResult;
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({ message: 'ada yang salah di controller joinRoom' });
+    }
+  };
+
+  getSingleRoom = async (req, res) => {
+    const { id } = req.params;
+    try {
+      const singleRoom = await userModel.getSingleRoom(id);
+      return res.json(singleRoom);
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({ message: 'ada yang salah di controller getSingleRoom' });
+    }
+  };
+
+  singleUserHistory = async (req, res) => {
+    const { id } = req.token;
+    try {
+      const userHistory = await userModel.singleUserHistory(id);
+      return res.json(userHistory);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: 'ada error di controller singleUserHistory' });
+    }
+  };
+
+  getAllRoom = async (req, res) => {
+    try {
+      const allRooms = await userModel.getAllRoom();
+      return res.json(allRooms);
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({ message: 'ada kesalahan di controller getAllRoom' });
+    }
+  };
+
+  playerVsCom = async (req, res) => {
+    const { id } = req.token;
+    const { choicePlayer1 } = req.body;
+
+    try {
+      // create room dan rekam pilihan player 1
+      const roomName = 'VS COM';
+      const choicePlayer2 = judgementRPS.vsCom();
+      const [resultPlayer1, resultPlayer2] = judgementRPS.judgementRPS(
+        choicePlayer1,
+        choicePlayer2,
+      );
+
+      // buat agar inputan hanya berupa batu kertas atau gintung
+      if (
+        choicePlayer1 !== 'batu'
+        && choicePlayer1 !== 'kertas'
+        && choicePlayer1 !== 'gunting'
+      ) {
+        return res
+          .status(400)
+          .json({ message: 'Format harus berupa batu / kertas / gunting' });
+      }
+
+      await userModel.playerVsCom(
+        roomName,
+        id,
+        choicePlayer1,
+        resultPlayer1,
+        choicePlayer2,
+        resultPlayer2,
+      );
+      return res.json({
+        message: 'berhasil membuat room playerVsCom',
+        'pilihan-mu': choicePlayer1,
+        'pilihan-computer': choicePlayer2,
+        'hasil-pilihanmu': resultPlayer1,
+        'hasil-computer': resultPlayer2,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({
+        message: 'pilihan harus berupa batu / kertas / gunting',
+      });
+    }
+  };
+
+  generatePdf = async (req, res) => {
+    const { id } = req.token;
+    const histories = await userModel.singleUserHistory(id);
+    const gameHistories = histories.map((room) => ({
+      room_id: room.room_id,
+      roomName: room.roomName,
+      updatedAt: moment(room.updatedAt).format('MMM DD, YYYY, h:mm A'),
+      hasilPlayer: room.hasilPlayer,
+    }));
+    const pdfDoc = new PDFDocument({ size: 'A4', margin: 20 });
+    const pdfPath = `output${id}.pdf`;
+    const file = bucket.file(pdfPath);
+    const stream = file.createWriteStream();
+    pdfDoc.pipe(stream);
+
+    pdfDoc.fontSize(12).text('Game Histories:', 60, 100);
+
+    const tableTop = 150;
+    const colWidths = [80, 200, 150, 100];
+    const columns = ['Room Id', 'Room Name', 'Date', 'Result'];
+
+    let xPos = 60;
+    let yPos = tableTop;
+
+    const addPageIfNeeded = () => {
+      if (yPos > 750) {
+        pdfDoc.addPage();
+        yPos = 50;
+      }
     };
 
-    userRegister = async (req, res) => {
-        try {
-            const requestData = matchedData(req); // Test record data in express-validator
-            const hasilData = await userModel.isUserAvail(requestData); // check data in database
-            if (hasilData) {
-                return res.send({message: "User / Email not available"});
-            }
-            // Record data to database
-            userModel.recordNewData(requestData);
-            return res.json({message: "Registration success"});
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({message: "Error registering user"});
-        }
+    const drawHeaders = () => {
+      xPos = 60;
+      columns.forEach((col, index) => {
+        pdfDoc.text(col, xPos, yPos);
+        xPos += colWidths[index];
+      });
+      yPos += 30;
     };
 
-    // login check
-    userLogin = async (req, res) => {
-        try {
-            const {userOrEmail, password} = req.body;
-            const hasilLogin = await userModel.checkUserLogin(
-                userOrEmail,
-                password
-            );
+    drawHeaders();
 
-            if (hasilLogin) {
-                // Generate JWT with token
-                const token = jwtApp.sign(
-                    {...hasilLogin, role: "player"},
-                    process.env.SECRET_KEYS,
-                    {expiresIn: "1d"}
-                );
-                return res.json({accessToken: token});
-            } else {
-                return res.json({message: "Credential not found"});
-            }
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({message: "Error logging in user"});
-        }
-    };
+    gameHistories.forEach((history) => {
+      addPageIfNeeded();
 
-    // check user detail
-    userDetail = async (req, res) => {
-        try {
-            const {idUser} = req.params;
-            const users = await userModel.getProfileUser(idUser);
+      xPos = 60;
+      pdfDoc.text(history.room_id, xPos, yPos); xPos += colWidths[0];
+      pdfDoc.text(history.roomName, xPos, yPos); xPos += colWidths[1];
+      pdfDoc.text(history.updatedAt, xPos, yPos); xPos += colWidths[2];
+      pdfDoc.text(history.hasilPlayer, xPos, yPos);
 
-            if (users) {
-                return res.json(users);
-            } else {
-                return res
-                    .status(400)
-                    .json({message: `User ${idUser} not found`});
-            }
-        } catch (error) {
-            console.log(error);
-            return res
-                .status(500)
-                .json({message: "Error retrieving user detail"});
-        }
-    };
+      yPos += 20;
 
-    // Update User Profile
-    userUpdate = async (req, res) => {
-        try {
-            const {idUser} = req.params;
-            const {fullName, address, phoneNumber} = req.body;
-            const user = await userModel.getSingleUser(idUser);
-            if (!user) {
-                return res.status(400).json({message: "User not found"});
-            }
+      if (yPos > 750) {
+        addPageIfNeeded();
+        drawHeaders();
+      }
+    });
 
-            const updatedUser = await userModel.updateSingleUser(
-                fullName,
-                address,
-                phoneNumber,
-                idUser
-            );
+    pdfDoc.end();
 
-            if (!updatedUser) {
-                return res.status(400).json({message: "Failed to update user"});
-            }
-
-            return res.json(updatedUser);
-        } catch (error) {
-            console.log(error);
-            return res
-                .status(500)
-                .json({message: "Error updating user profile"});
-        }
-    };
+    stream.on('finish', async () => {
+      const url = await file.getSignedUrl({
+        action: 'read',
+        expires: '03-01-2500',
+      });
+      res.json({
+        pdfUrl: url[0],
+      });
+    });
+  };
 }
 
-module.exports = new userController();
+module.exports = new UserController();
--- users/user.model.js
+++ users/user.model.js
@@ -1,107 +1,249 @@
-const md5 = require("md5");
-const db = require("../db/models");
-const {Op} = require("sequelize");
+/* eslint-disable consistent-return */
+/* eslint-disable class-methods-use-this */
 
-class userModel {
-    // cek all user
-    getAllUser = async () => {
-        const userList = await db.User.findAll({
-            include: [db.UserBio],
-            attributes: {exclude: ["password"]},
-        });
-        return userList;
-    };
+const md5 = require('md5');
+const { Op } = require('sequelize');
+const db = require('../db/models');
 
-    // cek single user
-    getSingleUser = async idUser => {
-        try {
-            return await db.User.findOne({
-                where: {id: idUser},
-                attributes: {exclude: ["password"]},
-            });
-        } catch (error) {
-            throw new Error("Failed to get user information: " + error.message);
-        }
-    };
+class UserModel {
+  async getAllUser() {
+    const allUser = await db.Users.findAll({
+      raw: true,
+      include: [db.User_Bios],
+      attributes: {
+        exclude: ['password'],
+      },
+    });
+    // console.log(allUser);
+    return allUser;
+  }
 
-    // check detail user bio
-    getProfileUser = async idUser => {
-        try {
-            return await db.User.findOne({
-                where: {id: idUser},
-                include: [db.UserBio],
-                attributes: {exclude: ["password"]},
-            });
-        } catch (error) {
-            throw new Error("Failed to get user profile: " + error.message);
-        }
-    };
+  async registerNewUser(userData) {
+    const createUser = await db.Users.create({
+      username: userData.username,
+      email: userData.email,
+      password: md5(userData.password),
+    });
+    return createUser;
+  }
 
-    //update single user
-    updateSingleUser = async (fullName, address, phoneNumber, idUser) => {
-        try {
-            const idUpdate = parseInt(idUser);
-            console.log(idUpdate);
-            const [UserBio, created] = await db.UserBio.upsert(
-                {
-                    id: idUpdate,
-                    fullName: fullName,
-                    phoneNumber: phoneNumber,
-                    address: address,
-                    user_id: idUpdate,
-                },
-                {returning: true}
-            );
-            return UserBio;
-        } catch (error) {
-            throw new Error("Failed to update user: " + error.message);
-        }
-    };
-    // cek is registered or not
-    isUserAvail = async queryUser => {
-        try {
-            const availUser = await db.User.findOne({
-                where: {
-                    [Op.or]: [
-                        {username: queryUser.username},
-                        {email: queryUser.email},
-                    ],
-                },
-            });
+  async isUserExist(userData) {
+    const existUser = await db.Users.findOne({
+      where: {
+        [Op.or]: [{ username: userData.username }, { email: userData.email }],
+      },
+    });
+    return !!existUser;
+  }
 
-            return availUser ? true : false;
-        } catch (error) {
-            throw new Error(
-                "Failed to check user availability: " + error.message
-            );
-        }
-    };
+  async loginUser(username, password) {
+    try {
+      const dataUser = await db.Users.findOne({
+        where: { username, password: md5(password) },
+        attributes: { exclude: ['password'] },
+        raw: true,
+      });
+      return dataUser;
+    } catch (error) {
+      // console.log(error);
+      // throw new Error('Ada yang salah di model login');
+    }
+  }
 
-    // record registration
-    recordNewData = queryUser => {
-        db.User.create({
-            username: queryUser.username,
-            email: queryUser.email,
-            password: md5(queryUser.password),
-        });
-    };
+  async uploadProfile(link, token) {
+    const uploadProfile = await db.Profile_Picture.create({
+      link,
+      user_id: token,
+    });
+    return uploadProfile;
+  }
 
-    // Cek Login
-    checkUserLogin = async (userOrEmail, password) => {
-        try {
-            const dataUser = await db.User.findOne({
-                where: {
-                    [Op.or]: [{username: userOrEmail}, {email: userOrEmail}],
-                    password: md5(password),
-                },
-                attributes: {exclude: ["password"]},
-                raw: true,
-            });
-            return dataUser;
-        } catch (error) {
-            throw new Error("Failed to check user login: " + error.message);
-        }
-    };
+  async updateProfile(link, token) {
+    await db.Profile_Picture.update({ link }, { where: { user_id: token } });
+  }
+
+  async isProfileExist(token) {
+    const profileExist = await db.Profile_Picture.findOne({
+      where: { user_id: token },
+    });
+    return !!profileExist;
+  }
+
+  async registerUserBio(
+    userId,
+    fullname,
+    address,
+    phoneNumber,
+    profilePicture,
+  ) {
+    const updateBio = await db.User_Bios.create({
+      fullname,
+      address,
+      phoneNumber,
+      user_id: userId,
+      profilePicture,
+    });
+    return updateBio;
+  }
+
+  async updateUserBio(token, fullname, address, phoneNumber, profilePicture) {
+    await db.User_Bios.update(
+      {
+        fullname,
+        address,
+        phoneNumber,
+        profilePicture,
+      },
+      { where: { user_id: token } },
+    );
+  }
+
+  async isUserBioExist(token) {
+    const bioExist = await db.User_Bios.findOne({
+      where: { user_id: token },
+    });
+
+    return !!bioExist;
+  }
+
+  async findUser(token) {
+    return db.Users.findOne({
+      include: [db.User_Bios, db.Profile_Picture],
+      where: { id: token },
+      attributes: { exclude: ['password'] },
+    });
+  }
+
+  async createRoom(player1, token) {
+    // console.log(player1);
+    const createRoomPlayer = await db.Game_Rooms.create({
+      roomName: player1.roomName,
+      idPlayer1: token,
+      choicePlayer1: player1.choicePlayer1,
+      statusRoom: 'Available',
+    });
+    return createRoomPlayer;
+  }
+
+  joinRoom(player2, token, id) {
+    const roomId = parseInt(id, 10);
+    db.Game_Rooms.update(
+      {
+        choicePlayer2: player2.choicePlayer2,
+        idPlayer2: token,
+      },
+      { where: { id: roomId } },
+    );
+  }
+
+  async getSingleRoom(id) {
+    const roomId = parseInt(id, 10);
+    const singleRoom = await db.Game_Rooms.findOne({
+      where: { id: roomId },
+      include: [
+        {
+          model: db.Users,
+          as: 'player1',
+          attributes: ['id', 'username'],
+        },
+        {
+          model: db.Users,
+          as: 'player2',
+          attributes: ['id', 'username'],
+        },
+      ],
+      raw: true,
+    });
+    return singleRoom;
+  }
+
+  async RoomResult(result, id) {
+    const roomId = parseInt(id, 10);
+    await db.Game_Rooms.update(
+      {
+        resultPlayer1: result[0],
+        resultPlayer2: result[1],
+        statusRoom: 'Finish',
+      },
+      { where: { id: roomId } },
+    );
+  }
+
+  async singleUserHistory(id) {
+    try {
+      const roomList = await db.Game_Rooms.findAll({
+        attributes: [
+          'id',
+          'roomName',
+          'updatedAt',
+          'idPlayer1',
+          'idPlayer2',
+          'resultPlayer1',
+          'resultPlayer2',
+        ],
+        where: {
+          [Op.or]: [{ idPlayer1: id }, { idPlayer2: id }],
+          [Op.and]: [
+            { resultPlayer1: { [Op.ne]: null } },
+            { resultPlayer2: { [Op.ne]: null } },
+          ],
+        },
+        raw: true,
+      });
+
+      return roomList.map((room) => ({
+        room_id: room.id,
+        player_id: room.idPlayer1 === id ? room.idPlayer1 : room.idPlayer2,
+        hasilPlayer:
+          room.idPlayer1 === id ? room.resultPlayer1 : room.resultPlayer2,
+        roomName: room.roomName,
+        updatedAt: room.updatedAt,
+      }));
+    } catch (error) {
+      // throw new Error(`Gagal menerima room detail : ${error.message}`);
+    }
+  }
+
+  async getAllRoom() {
+    const allRoom = await db.Game_Rooms.findAll({
+      include: [
+        {
+          model: db.Users,
+          as: 'player1',
+        },
+        {
+          model: db.Users,
+          as: 'player2',
+        },
+      ],
+    });
+    return allRoom;
+  }
+
+  async playerVsCom(
+    roomName,
+    id,
+    choicePlayer1,
+    resultPlayer1,
+    choicePlayer2,
+    resultPlayer2,
+  ) {
+    try {
+      const playerVSCom = await db.Game_Rooms.create({
+        roomName,
+        idPlayer1: id,
+        idPlayer2: 2,
+        choicePlayer1,
+        resultPlayer1,
+        choicePlayer2,
+        resultPlayer2,
+        statusRoom: 'Finish',
+      });
+      return playerVSCom;
+    } catch (error) {
+      // console.log(error);
+    }
+  }
 }
 
-module.exports = new userModel();
+module.exports = new UserModel();
