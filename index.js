3e33d3e904bb9c60cf07d098b5ab28b7a4ba2dcb
Rifki Nugraha <rifkichocobo@gmail.com>
2023-09-12 21:27:43 +0800

    chore : delete API folder for vercel deploy

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app = express();
 const port = 8000;
 const usersRoute = require("./users/user.route");
 const gameRoute = require("./game/game.route");
-// const cors = require("cors");
+const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./gameRpsSwagger.json");
 require("dotenv").config();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
