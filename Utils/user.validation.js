7e63e47b3f2e3277826414a6757470a5bf6703db
Rifki Nugraha <rifkichocobo@gmail.com>
2023-09-12 21:51:52 +0800

    fix : register schema for user friendly

--- Utils/user.validation.js
+++ Utils/user.validation.js
@@ -1,105 +1,80 @@
-const {body} = require("express-validator");
+const { body } = require('express-validator');
 
 const userRegistrationValidation = [
-    body("email")
-        .notEmpty()
-        .withMessage("Please enter the password")
-        .isEmail()
-        .withMessage("Invalid email format")
-        .isLength({min: 5, max: 50})
-        .withMessage("Email Min 6 Max 15 characters")
-        .matches(/^[a-zA-Z0-9@._]+$/)
-        .withMessage("Email can only contain letters, numbers, @, ., and _"),
-    body("password")
-        .notEmpty()
-        .withMessage("Please enter the password")
-        .isLength({min: 8, max: 15})
-        .withMessage("Password Min 6 Max 15 characters")
-        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d\s]+$/)
-        .withMessage(
-            "Password can contain letters, numbers, and Special Character"
-        ),
-    body("username")
-        .notEmpty()
-        .withMessage("Please enter the username")
-        .isString()
-        .withMessage("Must be character")
-        .isLength({min: 5, max: 50})
-        .withMessage("Username/Email Min 6 Max 15 characters")
-        .matches(/^[a-zA-Z0-9@._]+$/)
-        .withMessage("Username can only contain letters, numbers, @, ., and _"),
+  body('email')
+    .notEmpty()
+    .withMessage('Please enter the password')
+    .isEmail()
+    .withMessage('Invalid email format')
+    .isLength({ min: 5, max: 50 })
+    .withMessage('Email Min 6 Max 15 characters')
+    .matches(/^[a-zA-Z0-9@._]+$/)
+    .withMessage('Email can only contain letters, numbers, @, ., and _'),
+  body('password')
+    .notEmpty()
+    .withMessage('Please enter the password')
+    .isLength({ min: 6, max: 15 })
+    .withMessage('Password Min 6 Max 15 characters')
+    .matches(/^[A-Za-z0-9]+$/)
+    .withMessage('Password can contain letters, numbers, and Special Character'),
+  body('username')
+    .notEmpty()
+    .withMessage('Please enter the username')
+    .isString()
+    .withMessage('Must be character')
+    .isLength({ min: 5, max: 50 })
+    .withMessage('Username/Email Min 6 Max 15 characters')
+    .matches(/^[a-zA-Z0-9@._]+$/)
+    .withMessage('Username can only contain letters, numbers, @, ., and _'),
 ];
 
 const userLoginValidation = [
-    body("userOrEmail")
-        .notEmpty()
-        .withMessage("Please enter the username")
-        .isString()
-        .withMessage("Must be character")
-        .isLength({min: 5, max: 50})
-        .withMessage("Username/Email Min 6 Max 15 characters")
-        .matches(/^[a-zA-Z0-9@._]+$/)
-        .withMessage(
-            "User/Email can only contain letters, numbers, @, ., and _"
-        ),
-
-    // body("userOrEmail")
-    //     .notEmpty()
-    //     .withMessage("Please enter the username or email")
-    //     .custom(value => {
-    //         if (value.includes("@")) {
-    //             if (!validator.isEmail(value)) {
-    //                 throw new Error("Invalid email format");
-    //             }
-    //         } else {
-    //             if (!validator.isAlphanumeric(value)) {
-    //                 throw new Error(
-    //                     "Username can only contain letters and numbers"
-    //                 );
-    //             }
-    //         }
-    //         return true;
-    //     })
-    //     .withMessage("Invalid username or email format"),
-    body("password")
-        .notEmpty()
-        .withMessage("Please enter the password")
-        .isLength({min: 8, max: 15})
-        .withMessage("Password Min 6 Max 15 characters"),
+  body('userOrEmail')
+    .notEmpty()
+    .withMessage('Please enter the username')
+    .isString()
+    .withMessage('Must be character')
+    .isLength({ min: 5, max: 50 })
+    .withMessage('Username/Email Min 6 Max 15 characters')
+    .matches(/^[a-zA-Z0-9@._]+$/)
+    .withMessage('User/Email can only contain letters, numbers, @, ., and _'),
+  body('password')
+    .notEmpty()
+    .withMessage('Please enter the password')
+    .isLength({ min: 8, max: 15 })
+    .withMessage('Password Min 6 Max 15 characters'),
 ];
 
 const updateBioValidation = [
-    body("phoneNumber")
-        .notEmpty()
-        .withMessage("Please enter the phone number")
-        .isMobilePhone()
-        .withMessage("Please enter correct Number")
-        .isLength({min: 10, max: 15})
-        .withMessage("Phone number must be between 10 and 15 characters"),
-    body("address")
-        .notEmpty()
-        .withMessage("Please enter the address")
-        .isString()
-        .withMessage("Must be character")
-        .matches(/^[a-zA-Z0-9\s./]+$/)
-        .withMessage(
-            "Address can only contain letters, numbers, spaces, dots, and slashes"
-        )
-        .isLength({min: 5, max: 100})
-        .withMessage("Address must be between 5 and 100 characters"),
-    body("fullName")
-        .notEmpty()
-        .withMessage("Please enter the fullname")
-        .isString()
-        .withMessage("Must be character")
-        .isLength({min: 3, max: 50})
-        .withMessage("Fullname Min 3 Max 50 characters")
-        .matches(/^[a-zA-Z]+$/)
-        .withMessage("Fullname can only contain letters"),
+  body('phoneNumber')
+    .notEmpty()
+    .withMessage('Please enter the phone number')
+    .isMobilePhone()
+    .withMessage('Please enter correct Number')
+    .isLength({ min: 10, max: 15 })
+    .withMessage('Phone number must be between 10 and 15 characters'),
+  body('address')
+    .notEmpty()
+    .withMessage('Please enter the address')
+    .isString()
+    .withMessage('Must be character')
+    .matches(/^[a-zA-Z0-9\s./]+$/)
+    .withMessage('Address can only contain letters, numbers, spaces, dots, and slashes')
+    .isLength({ min: 5, max: 100 })
+    .withMessage('Address must be between 5 and 100 characters'),
+  body('fullName')
+    .notEmpty()
+    .withMessage('Please enter the fullname')
+    .isString()
+    .withMessage('Must be character')
+    .isLength({ min: 3, max: 50 })
+    .withMessage('Fullname Min 3 Max 50 characters')
+    .matches(/^[a-zA-Z]+$/)
+    .withMessage('Fullname can only contain letters'),
 ];
 
 module.exports = {
-    userRegistrationValidation,
-    userLoginValidation,
-    updateBioValidation,
+  userRegistrationValidation,
+  userLoginValidation,
+  updateBioValidation,
 };
