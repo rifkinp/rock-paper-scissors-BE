987c0f852f8303ffa07d6bbf7df918bf15f20568
Rifki Nugraha <rifkichocobo@gmail.com>
2023-09-12 23:16:45 +0800

    refactor :: simplify route, refactor unused route

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+const userRouter = require('./user/user.route');
+
 const app = express();
-const port = 8000;
-const usersRoute = require("./users/user.route");
-const gameRoute = require("./game/game.route");
-const cors = require("cors");
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./gameRpsSwagger.json");
-require("dotenv").config();
 
-app.use(cors());
+require('dotenv').config();
+
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  }),
+);
+
+app.options('*', cors());
+
 app.use(express.json());
-app.use(express.static("public"));
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.get("/", (req, res) => {
-  return res.json({message: "welcome to the jungle"});
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.get("/pong", (req, res) => {
-  return res.json({message: "PINGGG POING"});
-});
+app.use('/user', userRouter);
 
-app.use("/games", gameRoute);
-
-app.use("/users", usersRoute);
-
-app.listen(port, () => {
-  console.log("App is running on port " + port);
-});
+module.exports = app;
